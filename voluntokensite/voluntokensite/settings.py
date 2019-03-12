b2585ad716f16d3ab9d5573a4a3ff7b1d396e461
Phionx <shanjha.usa@gmail.com>
2019-03-12 19:03:07 -0400

    test

--- voluntokensite/voluntokensite/settings.py
+++ voluntokensite/voluntokensite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://voluntoken-dev.herokuapp.com']
+ALLOWED_HOSTS = ['https://voluntoken.herokuapp.com']
 
 
 # Application definition
