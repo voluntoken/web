4672b65f2ce65c6319fdc6bfceb5a473d53a64dc
Phionx <shanjha.usa@gmail.com>
2019-05-21 20:02:40 -0400

    clear

--- voluntokensite/NGO/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import org, event, checks_stub, event_registration_stub, event_hours_spent_stub
-# Register your models here.
-
-admin.site.register(org)
-admin.site.register(event)
-admin.site.register(checks_stub)
-admin.site.register(event_registration_stub)
-admin.site.register(event_hours_spent_stub)
